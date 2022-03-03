235a3f6033df9d7c6e91bb9ab459a24b8c44be51
Claudio Lopez <claudiols1979@hotmail.com>
2022-03-02 18:42:05 -0600

    add files

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# ubuntu-cleanup
-Ansible playbook to cleanup ubuntu 20+ server 
+# ansible_clean_ubuntu
+Ansible playbook to maintain Ubuntu 20.04 LTS server clean
---
+++ filter_plugins/splitpart.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+def splitpart (value, index, char = ','):
+    if isinstance(value, (list, tuple)):
+        ret = []
+        for v in value:
+            ret.append(v.split(char)[index])
+        return ret
+    else:
+        return value.split(char)[index]
+
+class FilterModule(object):
+    def filters(self):
+        return {'splitpart': splitpart}
+
+        
